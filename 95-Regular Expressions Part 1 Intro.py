03b24064ee7e471e2544be383ad1ed9680b255cf
Hussien Yasser <husienyaser2004@gmail.com>
2026-09-13 22:59:33 +0200

    95-Regular Expressions Part 1 Intro.py

---
+++ 95-Regular Expressions Part 1 Intro.py
@@ -0,0 +1,11 @@
+#-------------------------------------------------------------------------------------------------
+#----Regular Expressions => Intro-----------------------------------------------------------------
+#-------------------------------------------------------------------------------------------------
+# [1] Sequeces of characters That Define A Search Pattern
+# [2] Regular Expressions is Not in Python Its General Concept
+# [3] Used In [Credit Card Validation, Email Validation, IP Address Validation, Email Validation,]
+# [4] Test RegEx "https://Pythex.org"
+# [5] Characters Sheet "https://www.debuggex.com/cheatsheet/regex/python"
+#--------------------------------------------------------------------------------------------------
+
+
